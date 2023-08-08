5bbf20acc01421059334d23336d96649f4b574e2
Roman Lebedynskyi <108210477+Anasurim@users.noreply.github.com>
2023-08-09 00:54:49 +0200

    created help. sendEmail, updated register with verification token

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,10 +1,13 @@
 const { User } = require("../models/user");
+const { nanoid } = require("nanoid");
 
 const bcrypt = require("bcrypt");
 
 const gravatar = require("gravatar");
 
-const { HttpError } = require("../helpers");
+const { HttpError, sendEmail } = require("../helpers");
+
+const { BASE_URL } = process.env;
 
 const register = async (req, res, next) => {
   try {
@@ -19,11 +22,22 @@ const register = async (req, res, next) => {
 
     const avatarURL = gravatar.url(email);
 
+    const verificationToken = nanoid();
+
     const newUser = await User.create({
       ...req.body,
       password: hashPassword,
       avatarURL,
+      verificationToken,
     });
+
+    const verifyEmail = {
+      to: email,
+      subject: "Verify email",
+      html: `<a target="_blank" href= "${BASE_URL}/api/auth/verify/${verificationToken}">Click verify email</a>`,
+    };
+
+    await sendEmail(verifyEmail);
 
     res.status(201).json({
       email: newUser.email,
---
+++ helpers/sendEmail.js
@@ -0,0 +1,34 @@
+const nodemailer = require("nodemailer");
+
+require("dotenv").config();
+
+const { GMAIL_PASSWORD } = process.env;
+
+const nodemailerConfig = {
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  auth: {
+    user: "cliffcrystall@gmail.com",
+    pass: GMAIL_PASSWORD,
+  },
+};
+
+const transport = nodemailer.createTransport(nodemailerConfig);
+
+const sendEmail = async (data) => {
+  try {
+    await transport.sendMail({
+      from: "cliffcrystall@gmail.com",
+      to: data.to,
+      subject: data.subject,
+      text: data.text,
+    });
+
+    console.log("Email sent success");
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+module.exports = sendEmail;
